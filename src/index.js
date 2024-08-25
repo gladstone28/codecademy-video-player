2b02950366118c8a83af36e385bf73a07a4c0a16
Gladstone Rose <gladstonerose@hotmail.com>
2024-08-25 18:51:35 -0500

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
+
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './App'; // Ensure this matches the file name exactly
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 
